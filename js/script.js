c3ecc43f819f8877738a619f784fa5251709d0ff
Pawin <captainsuperman2011@gmail.com>
2022-05-10 23:01:50 +0700

    Fix computerPlay function and create playRound function

--- js/script.js
+++ js/script.js
@@ -1,5 +1,46 @@
-function computerPlay(){
-  let choice = ['Rock','Paper','Scissors'];
-  return choice[Math.floor(Math.random()*2)];
+function computerPlay() {
+  let choice = ['Rock', 'Paper', 'Scissors'];
+  return choice[Math.floor(Math.random() * 3)];
 }
-console.log(computerPlay())
+
+function playRound(playerSelection, computerSelection) {
+  playerSelection = playerSelection.toLowerCase()
+  if (playerSelection === "rock") {
+    if (computerSelection === "Rock") {
+      return "Tie";
+    }
+    else if (computerSelection === "Paper") {
+      return "You Lose! Paper Beats Rock";
+    }
+    else if (computerSelection === "Scissors") {
+      return "You Win! Rock Beats Scissors";
+    }
+  }
+  else if (playerSelection === "paper") {
+    if (computerSelection === "Rock") {
+      return "You Win! Paper Beats Rock";
+    }
+    else if (computerSelection === "Paper") {
+      return "Tie";
+    }
+    else if (computerSelection === "Scissors") {
+      return "You Lose! Scissors Beats Paper";
+    }
+  }
+  else if (playerSelection === "scissors") {
+    if (computerSelection === "Rock") {
+      return "You Lose! Rock Beats Scissors";
+    }
+    else if (computerSelection === "Paper") {
+      return "You Win! Scissors Beats Paper";
+    }
+    else if (computerSelection === "Scissors") {
+      return "Tie";
+    }
+  }
+}
+
+let playerSelection = "Rock";
+let computerSelection = computerPlay();
+console.log(computerSelection);
+console.log(playRound(playerSelection,computerSelection));
